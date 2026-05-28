fff479a1050afaadd2072e2a3a7889771eba83e7
unknown <your@email.com>
2026-05-28 16:07:58 -0500

    Lower threshold to 0.015, headline weight 40%, PYTHONDONTWRITEBYTECODE

--- run_bot.bat
+++ run_bot.bat
@@ -4,4 +4,5 @@ set OILBOT_DB=oilbot.db
 set EIA_API_KEY=mytqe9cWgvb30w7fYgIrAB6K2Dfq80e4VmKg9apL
 set NEWSAPI_KEY=b9571ec6725649ca9107ae1dca3e5665
 set PYTHONUNBUFFERED=1
+set PYTHONDONTWRITEBYTECODE=1
 start /B python -u -m oilbot.main > bot_out.log 2>&1
